25a1501da06273eea6e38d6a3bc89e3cbeae38f7
Colin Bookman <cobookman@gmail.com>
2014-03-27 19:35:25 -0400

    Added checkin route

--- api/router.php
+++ api/router.php
@@ -10,99 +10,106 @@ $app->post('/organization/memberPermission/:id', function($id) { User::permissio
 
 //Gets the current checkins
 $app->get('/checkin/:orgId/:meetingId', function($orgId, $meetingId) {
-    //Example 'invalid userId' given
-echo '
-  {
-    "checkins" : [
-        {
-         "id" : "cbookman3",
-         "major" : "CMPE",
-         "email" : "cbookman@gatech.edu", 
-         "affiliation" : "student",
-         "name" : {
-            "first" : "Colin",
-            "middle" : "Paul",
-            "last" : "Bookman",
-            "full" : "Bookman, Colin Paul"
-         },
-         "checkinTime" : "2014-03-27T15:20:51.869Z",
-         "isNew" : true
-        },
-        { "id" : "gburdell3",
-         "major" : "CMPE",
-         "email" : "cbookman@gatech.edu", 
-         "affiliation" : "student",
-         "name" : {
-            "first" : "Colin",
-            "middle" : "Paul",
-            "last" : "Bookman",
-            "full" : "Bookman, Colin Paul"
-         },
-         "checkinTime" : "2014-03-27T15:20:22.578Z",
-         "isNew" : false
-        }
-    ],
-    "statistics" : {
-        "attendance" : 53,
-        "rate"  : 2,
-        "newMembers" : 4
+    $checkin = new Checkin();
+    $output = array(
+        "checkins" => array(),
+        "statistics" => array()
+    );
+   
+    //Get list of records
+    $records= $checkin->getRecords($orgId, $meetingId, 25);
+    if($records) {
+        array_push($output["checkins"], $records);
     }
-  }';
+
+    //Get statistics on event
+    $statistics = $checkin->getStaistics($orgId, $meetingId);
+    if($statistics) {
+        array_push($output["statistics"], $statistics);  
+    }
+    echo json_encode($statistics);
 });
+
 $app->post('/checkin/:orgId/:meetingId/:userId', function($orgId, $meetingId, $userId) {
-  //Sample Output...also puts in a bad query (should always be @ top)
-  echo '
-  {
-    "checkins" : [
-        {
-         "id" : "'.$userId.'",
-         "major" : "CMPE",
-         "email" : "cbookman@gatech.edu", 
-         "affiliation" : "student",
-         "name" : {
-            "first" : "Colin",
-            "middle" : "Paul",
-            "last" : "Bookman",
-            "full" : "'.$userId.'"
-         },
-         "checkinTime" : "2014-03-27T15:20:51.869Z",
-         "isNew" : false
-        },
-        {
-         "id" : "cbookman3",
-         "major" : "CMPE",
-         "email" : "cbookman@gatech.edu", 
-         "affiliation" : "student",
-         "name" : {
-            "first" : "Colin",
-            "middle" : "Paul",
-            "last" : "Bookman",
-            "full" : "Bookman, Colin Paul"
-         },
-         "checkinTime" : "2014-03-27T15:20:51.869Z",
-         "isNew" : true
-        },
-        { "id" : "gburdell3",
-         "major" : "CMPE",
-         "email" : "cbookman@gatech.edu", 
-         "affiliation" : "student",
-         "name" : {
-            "first" : "Colin",
-            "middle" : "Paul",
-            "last" : "Bookman",
-            "full" : "Bookman, Colin Paul"
-         },
-         "checkinTime" : "2014-03-27T15:20:22.578Z",
-         "isNew" : false
-        }
-    ],
-    "statistics" : {
-        "attendance" : 53,
-        "rate"  : 2,
-        "newMembers" : 4
+    $checkin = new Checkin();
+    $output = array(
+        "checkins" => array(),
+        "statistics" => array()
+    );
+    //checkin user
+    if(!$checkin->guest($orgId, $meetingId, $userId)) { //error
+        array_push($output["checkins"],  array("invalid"=>true, "id"=>$userId));
     }
-  }';
+
+    //Get list of records
+    $records= $checkin->getRecords($orgId, $meetingId, 25);
+    if($records) {
+        array_push($output["checkins"], $records);
+    }
+
+    //Get statistics on event
+    $statistics = $checkin->getStaistics($orgId, $meetingId);
+    if($statistics) {
+        array_push($output["statistics"], $statistics);  
+    }
+    echo json_encode($statistics);
+
 });
+   
+// $app->post('/checkin/:orgId/:meetingId/:userId', function($orgId, $meetingId, $userId) {
+//   //Sample Output...also puts in a bad query (should always be @ top)
+//   echo '
+//   {
+//     "checkins" : [
+//         {
+//          "id" : "'.$userId.'",
+//          "major" : "CMPE",
+//          "email" : "cbookman@gatech.edu", 
+//          "affiliation" : "student",
+//          "name" : {
+//             "first" : "Colin",
+//             "middle" : "Paul",
+//             "last" : "Bookman",
+//             "full" : "'.$userId.'"
+//          },
+//          "checkinTime" : "2014-03-27T15:20:51.869Z",
+//          "isNew" : false
+//         },
+//         {
+//          "id" : "cbookman3",
+//          "major" : "CMPE",
+//          "email" : "cbookman@gatech.edu", 
+//          "affiliation" : "student",
+//          "name" : {
+//             "first" : "Colin",
+//             "middle" : "Paul",
+//             "last" : "Bookman",
+//             "full" : "Bookman, Colin Paul"
+//          },
+//          "checkinTime" : "2014-03-27T15:20:51.869Z",
+//          "isNew" : true
+//         },
+//         { "id" : "gburdell3",
+//          "major" : "CMPE",
+//          "email" : "cbookman@gatech.edu", 
+//          "affiliation" : "student",
+//          "name" : {
+//             "first" : "Colin",
+//             "middle" : "Paul",
+//             "last" : "Bookman",
+//             "full" : "Bookman, Colin Paul"
+//          },
+//          "checkinTime" : "2014-03-27T15:20:22.578Z",
+//          "isNew" : false
+//         }
+//     ],
+//     "statistics" : {
+//         "attendance" : 53,
+//         "rate"  : 2,
+//         "newMembers" : 4
+//     }
+//   }';
+// });
 $app->notFound(function () use ($app) {
    echo '{ "error" : "invalid API call"}';
 });
---
+++ api/routes/Checkin.php
@@ -0,0 +1,129 @@
+<?php
+Class Checkin {
+  public function __construct () {
+    require_once("./library/DB.php");
+    require_once("./library/GTED.php");
+    $this->gted = new GTED();
+    $this->userDb = new DB("users");
+    $this->checkinDb = new DB("checkin");
+  }
+  public function getStaistics($orgId, $meetingId) {
+    if(is_nan($orgId)) {
+      throw new Error("OrgId is not a number: " . $orgId); return;
+    }
+    $orgId = intval($orgId);
+    $sql = "SELECT COUNT(`timestamp`) FROM $org WHERE `meetingId` = :meetingId AND `timestamp` > DATE_SUB(NOW(), INTERVAL 6 HOURS)";
+    $attendance = $this->checkinDb->fetchAll($sql, array("meetingId"=>$meetingId), "COLUMN");
+    if($attendance) {
+      $attendance = $attendance[0][0]; //first record, first field
+    }
+  }
+  /*
+    Checks in a guest, returns true if successful, or false if unsucsessful
+  */
+  public function guest($orgId, $meetingId, $userId) {
+    if($this->isLoggedInOrg($orgId) && !$this->isCheckedIn($orgId, $meetingId, $userId)) {
+      return $this->checkInUser($orgId, $meetingId, $userId);
+    } else {
+      return false;
+    }
+  }
+  /*
+    Gets records in desc order
+  */
+  public function getRecords($orgId, $meetingId, $number) {
+    if(is_nan($orgId)) {
+      throw new Error("OrgId is not a number: " . $orgId); return;
+    }
+    $orgId = intval($orgId);
+    $number = intval($number);
+    $sql = "SELECT * FROM $orgId WHERE `meetingId` = :meetingId ORDER BY `timestamp` DESC LIMIT 0, $number";
+    $records = $this->checkinDb->fetchAll($sql, array("meetingId"=>$meetingId));
+    
+    //Check if user has ever checked into this organization
+    $existsQuery = $this->checkinDb->prepare("SELECT * FROM $orgId WHERE `meetingId` = :meetingId AND `userId` = :userId");
+    for($i=0; $i < count($records); ++$i) {
+      if($existsQuery->execute(array("meetingId"=>$meetingId, "userId"=>$record[$i]["userId"]))) {
+        if($existsQuery->rowCount() === 0) {
+          $records[$i]["isNew"] = true;
+        } else {
+          $records[$i]["isNew"] = false;
+        }
+      }
+    }
+    return $records;
+  }
+  /*
+    adds the gtusername associated with userId to our checkin table
+  */
+  private function checkInUser($orgId, $meetingId, $userId) {
+    if(is_nan($orgId)) {
+      throw new Error("OrgId is not a number: " . $orgId); return;
+    } 
+    $orgId = intval($orgId);
+    try {
+      $userId = $this->gtedUser($userId)["gtprimarygtaccountusername"][0];
+    } catch(Error $e) {
+      return false;
+    }
+    $sql = "INSERT INTO
+             $orgId (`userId`, `meetingId`, `timestamp`, `by`)
+            VALUES  (:userId ,  :meetingId,  now(), :by )
+           ";
+    if($this->checkinDb->query($sql, array("userId"=>$userId, "meetingId"=>$meetingId, "by"=>$GLOBAL["USERNAME"]))) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+  /*
+    Returns trus if the specified user hasn't checked into the meetingId 
+    in the past 6 hours
+  */
+  public function isCheckedIn($orgId, $meetingId, $userId) {
+    if(is_nan($orgId)) {
+      throw new Error("OrgId is not a number: " . $orgId); return;
+    }
+    $orgId = intval($orgId);
+    $sql = "SELECT * FROM $orgId WHERE `timestamp` > DATE_SUB(NOW(), INTERVAL 6 HOURS) AND `userId` = :userId";
+    $this->checkinDb->query($sql, array("userId" => $userId));
+    if($this->checkinDb->rowCount() === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  /*
+    Returns true if logged in user belogns to said org id
+            false if logged in user doesn't belong to said org id
+  */
+  public function isLoggedInOrg($orgId) {
+    $sql = "SELECT * FROM `users` WHERE `userId` = :userId AND `orgId` = :orgId";
+    $this->userDb->query($sql, array("orgId"=>$orgId, "userId"=>$GLOBAL["USERNAME"]));
+    if($this->userDb->rowCount() >= 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  /*
+    Returns the GTED LDAP results for any gtid, gtusername, or buzzcard id
+  */
+  public function gtedUser($userId) {
+    $output = null;
+    if(is_numeric($userId)) { //must be a buzzcard or gtid
+      $userId = strval($userId);
+      if(strlen($userId) >= 9) {
+        $output = $this->gted->queryGTID($userId);
+      } else {
+        $output = $this->gted->queryBuzzCard($userId);
+      }
+    } else { //its a gt-username
+      $output = $this->gted->queryGTUsername($userId);
+    }
+    return $output;
+  }
+}
+
+?>
